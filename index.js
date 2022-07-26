2d579d7f9dc8164a98bfd8f5a0384b099fd05daa
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-07-25 21:28:19 -0300

    Promises

--- index.js
+++ index.js
@@ -12,14 +12,4 @@ function pegaArquivo(caminhoDoArquivo) {
         .catch((erro) => trataErro(erro));
 }
 
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.readFile(caminhoDoArquivo,encoding,(erro,texto)=> {
-//         if(erro){
-//             trataErro(erro);
-//         }
-//         console.log(chalk.green(texto))
-//     });
-// }
-
 pegaArquivo('./arquivos/texto1.md');
