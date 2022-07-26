2b276cf491eec181aef724754714f48932fad778
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-07-25 21:27:21 -0300

    Promises

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
-function trataErro(erro){
-    throw new Error(chalk.red(erro.code,'Não há arquivo no caminho!'));
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho!'));
 }
 
 function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo,encoding,(erro,texto)=> {
-        if(erro){
-            trataErro(erro);
-        }
-        console.log(chalk.green(texto))
-    });
+    fs.promises.readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch((erro) => trataErro(erro));
 }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo,encoding,(erro,texto)=> {
+//         if(erro){
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto))
+//     });
+// }
 
 pegaArquivo('./arquivos/texto1.md');
