d9a6a20ebfc0c0984a692af21862f1965222aef0
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-07-25 21:15:30 -0300

    Tratamento de erros

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.red('vamos começar!'));
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code,'Não há arquivo no caminho!'));
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo,encoding,(erro,texto)=> {
+        if(erro){
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto))
+    });
+}
+
+pegaArquivo('./arquivos/texto1.md');
