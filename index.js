a5ad1587f4d6d3a95b317ae502de0aff267399e2
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-07-25 21:34:27 -0300

    Carregamento de Arquivos

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho!'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
+async function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
-    fs.promises.readFile(caminhoDoArquivo, encoding)
-        .then((texto) => console.log(chalk.green(texto)))
-        .catch((erro) => trataErro(erro));
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro);
+    }
 }
 
 pegaArquivo('./arquivos/texto1.md');
