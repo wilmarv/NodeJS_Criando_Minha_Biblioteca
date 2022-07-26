4399ac41745e82b4e7afce4db23abe5bbbf7120d
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-07-25 23:55:44 -0300

    Capturando Links

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+    while ((temp = regex.exec(texto)) !== null) {
+        arrayResultados.push({ [temp[1]]: temp[2] });
+    }
+    return arrayResultados;
+}
+
 function trataErro(erro) {
-    throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho!'));
+    throw new Error(chalk.red(erro.code, erro.message));
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
     try {
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(chalk.green(texto));
+        console.log(extraiLinks(texto));
     } catch (erro) {
         trataErro(erro);
     }
